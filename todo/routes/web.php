bbb552b4a4620aa1b7d6d27f377885f8e0670edc
DionCstS <dionathacastro1@gmail.com>
2024-10-17 23:45:27 -0300

    correção de sintake da Task

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get( uri: '/tasks', action: [taskController:: class, 'index']);
+Route::get( uri: '/tasks', action: [TaskController:: class, 'index']);
