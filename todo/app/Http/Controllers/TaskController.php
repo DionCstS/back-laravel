a7ead22207e2b82ccd5755093739494728302709
DionCstS <dionathacastro1@gmail.com>
2024-10-17 23:42:23 -0300

    modificação no controller

--- todo/app/Http/Controllers/TaskController.php
+++ todo/app/Http/Controllers/TaskController.php
@@ -8,9 +8,10 @@ class TaskController extends Controller
 {
     public function index()
     {
+        //Busca todas as tasks
         $tasks = Task::all();
 
-
+        //Retorna uma view com a lista de tarefas 
         return view('index', compact('tasks'));
     }
 }
