e7d9584d64702dabaf5664e10e09408cc6acc193
DionCstS <dionathacastro1@gmail.com>
2024-10-18 00:06:33 -0300

    correcao sintaxa

--- todo/routes/web.php
+++ todo/routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tasks', action: [TaskController:: class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index']);
